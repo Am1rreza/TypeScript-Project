9d0f07e19d727676915090ef54d3148851451d35
Amirreza <amirreza.m13825@gmail.com>
2023-08-08 23:50:59 +0330

    get the input elements from DOM

--- dist/app.js
+++ dist/app.js
@@ -6,7 +6,20 @@ class ProjectInput {
         const importedHTMLContent = document.importNode(this.templateElement.content, true);
         this.element = importedHTMLContent.firstElementChild;
         this.element.id = "user-input";
+        this.titleInputElement = this.element.querySelector("#title");
+        this.descriptionInputElement = this.element.querySelector("#description");
+        this.peopleInputElement = this.element.querySelector("#people");
+        this.configure();
         this.attach();
+    }
+    submitHandler(e) {
+        e.preventDefault();
+        const title = this.titleInputElement.value;
+        const description = this.descriptionInputElement.value;
+        const people = this.peopleInputElement.value;
+    }
+    configure() {
+        this.element.addEventListener("submit", this.submitHandler.bind(this));
     }
     attach() {
         this.hostElement.insertAdjacentElement("afterbegin", this.element);
